ddd591a92a806938f4a319b0294b8ca90ffd3999
adultlee <seoungin1228@gmail.com>
2023-01-09 17:20:03 +0900

    feat: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Interview-Space</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>
+          <h1>Main</h1>
+        </div>
+      </main>
+    </>
+  );
+}
