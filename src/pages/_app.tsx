f78db1e33e1eb0c7156c412c5e03fd6b50a7a892
Kimkyungmin123 <rud3302@naver.com>
2023-01-10 13:49:03 +0900

    chore: install storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from 'next/app';
 
+import { Global, ThemeProvider } from '@emotion/react';
+import { global } from 'src/styles/global';
+import theme from 'src/styles/theme';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
